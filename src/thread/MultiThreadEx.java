4b42fa0bf15717720bbd5e981cc29e34becf5928
T3AD20 <T3AD20@T3AD20C>
2017-01-19 15:32:44 +0900

    thread 연습

---
+++ src/thread/MultiThreadEx.java
@@ -0,0 +1,18 @@
+package thread;
+
+public class MultiThreadEx {
+
+	public static void main(String[] args) {
+		Thread t1 = new AlphabetThread();
+		Thread t2 = new DigitThread();
+		Thread t3 = new DigitThread();
+		Thread t4 = new Thread( new UpperCaseAlphabetThread() );
+		
+		t1.start();
+		t2.start();
+		t3.start();
+		t4.start();
+
+	}
+
+}
